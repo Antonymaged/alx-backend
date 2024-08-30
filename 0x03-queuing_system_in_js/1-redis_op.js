d9cfb96c72c3d188ebe4a124c2bbb32d04478e9e
Antonymaged <magedantony1@gmail.com>
2024-08-30 12:13:20 +0000

    1-redis_op.js

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,20 @@
+import createClient from 'redis';
+
+const client = createClient();
+
+function setNewSchool(schoolName, value) {
+	client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+	client.get(schoolName, (err, res) => {
+		if(err){
+			console.log(err)
+		} else {
+			console.log(res);
+		}
+	})
+}
+
+client.on('connect', () => {console.log('Redis client connected to the server')});
+client.on('error', (err) => {console.log(`Redis client not connected to the server: ${err}`)});
